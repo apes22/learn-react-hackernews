ff7e4f2dd5e987a0c3d14703a102fcc3fa412476
Maribel Duran <maribelduranucm@gmail.com>
2018-01-18 17:28:03 -0800

    Define js variables and render to JSX

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const helloWorld = 'Welcome to the Road to learn React';
+    const user = {
+        firstName: "Maribel",
+        lastName: "Duran"
+    };
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <h2>{helloWorld}</h2>
         </header>
         <p className="App-intro">
+        <h3>Welcome {user.firstName} {user.lastName}!</h3>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
