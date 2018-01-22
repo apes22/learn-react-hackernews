ff43c5d9ee782112de813b9b14f3230fb4d25fd2
Maribel Duran <maribelduranucm@gmail.com>
2018-01-22 12:45:40 -0800

    Use synthetic events and higher order functions in React

--- src/App.js
+++ src/App.js
@@ -28,6 +28,20 @@ const list = [
   },
 ]
 
+//higher order function does at least one of the following
+//1) takes one or more functions as arguments
+//2) returns a function as its result
+//ES5
+/*
+function isSearched(searchTerm){
+  return function(item){
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  }
+}
+*/
+//ES6
+const isSearched = searchTerm => item =>
+item.title.toLowerCase().includes(searchTerm.toLowerCase());
 //Declaring the App component, but it extends from another "component" class called Component 
 //extends is like inheritance in OOP. Used to pass over functionalities from one class to another class.
 //The Component class encapsulates all the implementation details of a React component. It enables developers to use classes as components in React
@@ -46,19 +60,27 @@ class App extends Component {
       //list: list,
       list,
       helloWorld: 'Welcome to the Road to learn React',
-      comment: "This is coming form the local state of this component woooo!"
+      comment: "This is coming form the local state of this component woooo!",
+      searchTerm: "",
     };
     this.onDismiss = this.onDismiss.bind(this);
     this.doSomething = this.doSomething.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
     //avoid defining the business logic inside the constructor because it makes it messy
    /* this.onClick = () =>{
       console.log(this);
     }
     */
   }
-
   doSomething(){
     console.log(this);
+    
+  }
+  //When using a handler in your element, you get access to the "synthetic" React
+  //event in your callback function's signature
+  onSearchChange(event){
+    console.log(event.target.value)
+    this.setState({ searchTerm: event.target.value})
   }
   onDismiss(id){
     const isNotId =  item => item.objectID !== id;
@@ -74,10 +96,16 @@ class App extends Component {
           <h1 className="App-title">{this.state.helloWorld}</h1>
         </header>
         <p className="App-intro">
+        <form>
+          <input 
+            type="text" 
+            onChange={this.onSearchChange}     
+          />
+        </form>
        <h4> Below is your predefined list:</h4>
         {        
           //Using the list from my local state in my component
-          this.state.list.map(item => 
+          this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
             <div key = {item.objectID}>
               <span>
                 <a href={item.url}>{item.title} </a>
