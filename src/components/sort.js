bf03d92e29bb583639d1b6aa26eab74abd06b4be
Maribel Duran <maribelduranucm@gmail.com>
2018-02-20 19:10:27 -0800

    Add arrow to sort header to indicate the sort

--- src/components/sort.js
+++ src/components/sort.js
@@ -2,10 +2,44 @@ import React from 'react';
 import Button from './button';
 import classNames from 'classnames';
 
+const Arrow = ({isReversed}) => isReversed 
+? <span className="fa fa-angle-up"></span>
+: <span className="fa fa-angle-down"></span>
+
 const Sort = ({
   sortKey,
   activeSortKey,
   onSort,
+  isSortReverse,
+  children,
+  }) => {
+  
+    const isActiveSortKey = (sortKey === activeSortKey);
+    const sortClass = classNames(
+      'button-inline',
+      { 'button-active': isActiveSortKey }
+    );
+    
+    return (
+    <Button
+      onClick={() => onSort(sortKey)}
+      className={sortClass}
+      >
+      {children}
+      { isActiveSortKey ? 
+      <Arrow isReversed={isSortReverse} />
+      : null
+      }
+    </Button>
+    )
+}
+
+/*
+const Sort = ({
+  sortKey,
+  activeSortKey,
+  onSort,
+  isSortReverse,
   children
   }) => {
   const sortClass = classNames(
@@ -17,9 +51,11 @@ return (
   onClick={() => onSort(sortKey)}
   className={sortClass}
   >
+  <Arrow isReversed={isSortReverse} />
   {children}
   </Button>
 );
 }
+*/
 
 export default Sort;
